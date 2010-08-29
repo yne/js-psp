dbb820b45a23205221106ca7eca660b36723bc8f
yne <yne@users.noreply.github.com>
2010-08-29 10:36:36 +0000

    first step to allow the interpreter to load JS kernel libs (part 2)

--- prx/kernel/main.c
+++ prx/kernel/main.c
@@ -1,39 +1,29 @@
 #include <pspkernel.h>
+#include <pspkdebug.h>
 #include <stdio.h>
+#include <string.h>
+#include <systemctrl.h>
 
 #include "../../main/shared.h"
 
-PSP_MODULE_INFO("modK",PSP_MODULE_KERNEL,0,0);
+PSP_MODULE_INFO("JSEK",0x1007,0,0);
 
-int Kfun(void){
-	printf("[modK] Kfun called\n");
-	return 0;
+KmodInfo js_addKModule(void){
+	KmodInfo info;
+	info.lfun = NULL;
+	info.gfun = NULL;
+	info.lvar = NULL;
+	info.gvar = NULL;
+	return info;
 }
-JSBool template(JSContext *cx, JSObject *gobj, uintN argc, jsval *argv, jsval *rval){
-	printf("test receive : \"%i\"",J2I(argv[0]));
-	*rval = I2J(J2I(argv[0])*3);
-	return JS_TRUE;
+
+int js_searchInfo(KfindProc chaine){
+	return sctrlHENFindFunction(chaine.mod, chaine.lib, chaine.nid);
+//	printf("%s\n\n",chaine.value);
 }
-static JSFunctionSpec functions[] = {
-	{"template",template, 1},
-	{0}
-};
-JSFunctionSpec* modK_GetFun(void){
-	return functions;
-}
-static JSPropertiesSpec var[] = {
-	{"KTEMPLATE_IS_LOADED",I2J(1)},
-	{0}
-};
-JSPropertiesSpec* modK_GetVar(void){
-	return var;
-}
+
 int module_start(SceSize args, char* argp[]){
-	printf("[modK] started !\n");
+	printf("Kloader ready\n\n");
 	//printf("[modK] call js_test : %08X\n",js_test(5));//forbidden by the os
 	return 0;//lib not started if != 0
-}
-int module_stop(SceSize args, void *argp){
-	printf("[modK] stoped !\n");
-	return 0;
 }
