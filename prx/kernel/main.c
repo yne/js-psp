dbf1d8d36f83c2ac1d1f7e756c8a9c7ca24b173c
yne <yne@users.noreply.github.com>
2010-10-31 01:17:43 +0000

    enhanced kernel agent

--- prx/kernel/main.c
+++ prx/kernel/main.c
@@ -7,25 +7,27 @@
 #include "../../main/shared.h"
 
 PSP_MODULE_INFO("JSEK",0x1007,0,0);
+PSP_MAIN_THREAD_ATTR(0);
 
-static JSPropertiesSpec var[] = {
-	{"Ksample", I2J(1)},
-	{0}
-};
-KmodInfo js_addKModule(void){
-	KmodInfo info;
-	info.lfun = NULL;
-	info.gfun = NULL;
-	info.lvar = NULL;
-	info.gvar = var;
-	return info;
+u32 loadModBuf(void*buffer,u32 size){
+	return 50;//sceKernelLoadModuleBuffer(size,(void*)buffer,0,NULL);
+	//u32 modid = sceKernelLoadModuleBuffer(sizeof(buffer),(void*)buffer,0,NULL);
+	//return sceKernelStartModule(modid, 0, NULL, NULL, NULL);
 }
-int tmpFunction(){//will be hoocked to the loaded *Kmodule*_addModule
+int my_addModule(){//will be hoocked to the loaded *Kmodule*_addModule
 	return 0;
 }
-int js_searchInfo(KfindProc chaine){
+u32 KCall(u32 uid,u32 nid,u32 arg){
+	SceModule2* mod = sceKernelFindModuleByUID(uid);
+	u32 (*function)(int);
+	function = (void*)sctrlHENFindFunction(mod->modname,mod->modname,nid);
+	if(function)
+		return function(arg);
+	return 0;
+}
+u32 js_searchInfo(KfindProc chaine){
 	u32 newFun = sctrlHENFindFunction(chaine.mod, chaine.lib, chaine.nid);
-	sctrlHENPatchSyscall((u32)tmpFunction,(void*)newFun);
+	sctrlHENPatchSyscall((u32)my_addModule,(void*)newFun);
 	return newFun;
 }
 int module_start(SceSize args, char* argp[]){
