3d7636f22e7b1e9a298739a956ed0746aafc5552
yne <yne@users.noreply.github.com>
2010-11-20 01:45:45 +0000

    new kernel function, fixed pandorize problem

--- prx/Ksample/KTool.js
+++ prx/Ksample/KTool.js
@@ -1,10 +1,16 @@
-var kernel = new KModule("prx/Ksample.prx");
-printf("FW :"+kernel.call("getDevkitVersion")+"\n");//getDevkitVersion = 0x4F764650 => printf("FW :"+kernel.call(0x4F764650)+"\n");
-//printf("bat serial ("+kernel.call("readSerial")+") \n");
-//printf("bat write  ("+kernel.call("writeSerial",0x12345678)+") \n");
-printf("Pommel ("+kernel.call("getPommelVersion")+") \n");
-printf("Baryon ("+kernel.call("getBaryonVersion")+") \n");
-printf("Tachyon("+kernel.call("getTachyonVersion")+") \n");
-printf("ori FW ("+kernel.call("getShippedFW")+") \n");
+var k = new KModule("prx/Ksample.prx");
+printf("act FW :"+k.call("getDevkitVersion").toString(16)+"\n");
+printf("shi FW :"+k.call("getShippedFW").toString(16)+"\n");
+printf("BatSer :"+k.call("getBatSerial")+"\n");
+printf("BatSer :"+k.call("setBatSerial",0x12345678)+"\n");//-1319368834
+printf("Tachyon:"+k.call("getTachyonVersion")+"\n");
+printf("Baryon :"+k.call("getBaryonVersion")+"\n");
+printf("Pommel :"+k.call("getPommelVersion")+"\n");
 
+printf("API    :"+k.call("getKernelInitApitype")+"\n");
+printf("model  :"+(k.call("getModel")?"slim+":"fat")+"\n");
+printf("SE     :"+k.call("getSE")+"\n");
+printf("DevHook:"+k.call("getDevhook")+"\n");
+printf("Version:"+k.call("getVersion").toString(16)+"\n");
 exit();
+ 
